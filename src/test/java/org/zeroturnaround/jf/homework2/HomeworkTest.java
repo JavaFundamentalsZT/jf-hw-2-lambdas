d61650cf1a7c725ed6b48cda9bc442ed6d2996c0
Arnel Pällo <arnelism@gmail.com>
2017-02-06 10:31:18 +0200

    Update HomeworkTest.java

--- src/test/java/org/zeroturnaround/jf/homework2/HomeworkTest.java
+++ src/test/java/org/zeroturnaround/jf/homework2/HomeworkTest.java
@@ -12,15 +12,15 @@ public class HomeworkTest {
 
   @Test
   public void testREADMEmd() throws IOException {
-    Map<String, Integer> expected = new LinkedHashMap<>();
-    expected.put("the", 28);
-    expected.put("of", 21);
-    expected.put("lorem", 19);
-    expected.put("ipsum", 19);
-    expected.put("it", 11);
+    Map<String, Long> expected = new LinkedHashMap<>();
+    expected.put("the", 28L);
+    expected.put("of", 21L);
+    expected.put("lorem", 19L);
+    expected.put("ipsum", 19L);
+    expected.put("it", 11L);
 
     Map<String, Long> actual = new Homework2().getWords("lorem-ipsum.txt");
-    assertEquals(expected.toString(), actual.toString());
+    assertEquals(expected, actual);
   }
 
 }
